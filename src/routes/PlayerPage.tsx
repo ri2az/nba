988537cc5192f3bb798039f394216652c7ab5142
Michael You <youmichaelc@gmail.com>
2022-12-02 15:34:02 -0800

    Add Stats type
    Now we can work with the stats for each player

--- src/routes/PlayerPage.tsx
+++ src/routes/PlayerPage.tsx
@@ -1,6 +1,29 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Stats } from "../types";
 
 export default function PlayerPage() {
   let { playerId } = useParams();
+  useEffect(() => {
+    fetch(`https://www.balldontlie.io/api/v1/stats?player_ids[]=${playerId}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(
+            `HTTP error: The status is ${response.status}`
+          );
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        setData(data.data);
+      })
+      .catch(err => setError(err))
+      .finally(() => setLoading(false));
+  }, []);
+  const [data, setData] = useState<Stats[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   return <>{playerId}</>;
 }
--- src/types.tsx
+++ src/types.tsx
@@ -18,3 +18,39 @@ export type Team = {
   full_name: string,
   name: string,
 }
+
+export type Stats = {
+  "id": number,
+  "ast": number,
+  "blk": number,
+  "dreb": number,
+  "fg3_pct": number,
+  "fg3a": number,
+  "fg3m": number,
+  "fg_pct": number,
+  "fga": number,
+  "fgm": number,
+  "ft_pct": number,
+  "fta": number,
+  "ftm": number,
+  "game": Game,
+  "min": string,
+  "oreb": number,
+  "pf": number,
+  "player": Player,
+  "pts": number,
+  "reb": number,
+  "stl": number,
+  "team": Team
+  "turnover": number
+};
+
+export type Game = {
+  "id": number,
+  "date": string,
+  "home_team_id": number,
+  "home_team_score": number,
+  "season": number,
+  "visitor_team_id": number,
+  "visitor_team_score": number
+};
